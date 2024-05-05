022a239b82fa2dfb3dcbf77f549e3f69b8bad62c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-05-05 13:29:36 +0530

    Making the paddle move

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	let ball = document.getElementById("ball"); // Targetting the ball element
 	let table = document.getElementById("ping-pong-table");
+	let paddle = document.getElementById("paddle"); // targetting the paddle element
 
 	//here the ballX and ballY will be helping us to set starting point of ball w.r.t table
 	let ballX = 50; // distance of the top of the ball w.r.t ping pong table
@@ -20,8 +21,26 @@ document.addEventListener("DOMContentLoaded", () => {
 		ball.style.left = `${ballX}px`;
 		ball.style.top = `${ballY}px`;
 
-		if (ballX > table.offsetWidth-ball.offsetWidth || ballX <= 0) dx *= -1; // change x-direction
-		if (ballY > table.offsetHeight-ball.offsetHeight || ballY <= 0) dy *= -1; // change y-direction
+		if (ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1; // change x-direction
+		if (ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1; // change y-direction
 	}, 1);
+
+
+	let paddleY = 0;
+	let dPy = 5; // displacement for paddle in y-direction
+
+	document.addEventListener("keydown", (event) => {
+		if (event.keyCode == 38 && paddleY > 0) {
+			// up arrow key is pressed
+			console.log("up");
+			paddleY += (-1)*dPy;;
+		} else if (event.keyCode == 40 && paddleY < table.offsetHeight - paddle.offsetHeight) {
+			// down arrow key is pressed
+			console.log("down");
+			paddleY += dPy;
+		}
+
+		paddle.style.top = `${paddleY}px`;
+	})
 
 })
